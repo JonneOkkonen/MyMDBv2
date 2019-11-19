80069a6d03f2b0e2e63cd889f8a18f8f873ec760
M2235 <M2235@student.jamk.fi>
2019-11-19 09:39:00 +0000

    Include site header and footer to layout

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -71,7 +71,33 @@
                 </div>
             </div>
         </nav>
-        @yield('content')
+        <!-- Page Header -->
+        <header class="masthead" style="background-image: url('{{ asset('img/home-bg.jpg') }}')">
+            <div class="overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-md-10 mx-auto">
+                            <div class="site-heading">
+                                @yield('heading')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </header>
+        <!-- Main Content -->
+        <main id="py-4">
+            @yield('content')
+        </main>
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-10 mx-auto">
+                        <p class="copyright text-muted">Copyright &copy; Jonne Okkonen 2018-2019</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,29 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
-<!-- Page Header -->
-    <header class="masthead" style="background-image: url('{{ asset('img/home-bg.jpg') }}')">
-        <div class="overlay"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-10 mx-auto">
-                        <div class="site-heading">
-                            <h1>MyMDB</h1>
-                            <span class="subheading">MyMovieDatabase</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </header>
-    <!-- Main Content -->
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <p class="copyright text-muted">Copyright &copy; Jonne Okkonen 2018-2019</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+@section('heading')
+    <h1>MyMDB</h1>
+    <span class="subheading">MyMovieDatabase</span>
 @endsection
