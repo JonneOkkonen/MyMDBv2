9bc76a49361aceb36755673c4e4c78c1c8f8a4e3
M2235 <M2235@student.jamk.fi>
2019-11-19 11:36:24 +0000

    Edit Home Page
    Add Welcome Message
    Add navbar items for Movies and TV-Shows
    Fix navbar DropDown menu

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,23 +1,8 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('heading')
+    <h1>Welcome</h1>
+    <span class="subheading">{{ Auth::user()->name }}</span>
+    <br>
+    <span class="subheading">Your are successfully logged in.</span>
 @endsection
