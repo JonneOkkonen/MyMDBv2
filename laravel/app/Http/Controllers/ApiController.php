bec4f008595dabeedd4a621eddd586249c0327f2
M2235 <M2235@student.jamk.fi>
2019-11-26 12:58:30 +0000

    Add MoviesCount to API
    Create /movies/count api route
    Create Count function to ApiController

--- laravel/app/Http/Controllers/ApiController.php
+++ laravel/app/Http/Controllers/ApiController.php
@@ -20,6 +20,23 @@ class ApiController extends Controller
         }
     }
 
+    public function count()
+    {
+        $response = self::CheckAPIKey(request('key'));
+        if($response == "APIKeyCorrect") {
+            $userID = DB::table('users')->where('api_token', request('key'))->value('id');
+            $sql = "SELECT IFNULL(type, 'All') as type, count(*) as count FROM movies WHERE userID = 1 GROUP BY type WITH ROLLUP";
+            $movies = DB::table('movies')
+                                ->selectRaw("IFNULL(type, 'All') as type, count(*) as count")
+                                ->where('userID', $userID)
+                                ->groupBy(DB::raw('type WITH ROLLUP'))
+                                ->get();
+            return response()->json($movies, 200);
+        }else {
+            return $response;
+        }
+    }
+
     public function CheckAPIKey($key) {
         if($key) {
             if(DB::table('users')->where('api_token', $key)->exists()) {
